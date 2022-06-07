4bea4e5d116e520a2e029b6a44807f259d81ff3c
Marcy Villegas <58241136+marcyvillegas@users.noreply.github.com>
2022-06-07 16:16:30 +0800

    added picture

--- learning-react/src/components/Navbar.js
+++ learning-react/src/components/Navbar.js
@@ -1,8 +1,15 @@
 
+import turtlePicture from '../images/tutle.jpg'
+
 export default function Navbar() {
     return (
         <nav className="navbar">
-            <h2>Navbar Sample</h2>
+
+            <div className="navbar__logo">
+                <img src={turtlePicture} alt="turtle" />
+                <h2>Navbar Sample</h2>
+            </div>
+            
             <ul className="navbar__list">
                 <li>Home</li>
                 <li>About</li>
