3a3349f01386ed3cb378acfb31cd613972ec3038
Marcy Villegas <58241136+marcyvillegas@users.noreply.github.com>
2022-06-12 14:22:16 +0800

    fetched user api using useEffect

--- learning-react/src/components/FetchApi.js
+++ learning-react/src/components/FetchApi.js
@@ -1,10 +1,30 @@
-
+import { useState, useEffect } from 'react';
 
 export default function FetchApi(){
 
+    // user state
+    const [userData, setUserData] = useState([])
+
+    // count state
+    const [count, setCount] = useState(1)
+
+    // side effect
+    useEffect(() => {
+        
+        console.log('EFFECT')
+        
+        fetch(`https://jsonplaceholder.typicode.com/users/${count}`)
+        .then(res => res.json())
+        .then(data => setUserData(data.name))
+
+    }, [count])
+
     return(
         <div style={{ padding: '0.5rem 5rem' }}>
-            <h1>Fetching API</h1>
+            <h1>Fetching API (using useEffect)</h1>
+            {userData}
+            <p>num: {count}</p>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
         </div>
     );
 }
